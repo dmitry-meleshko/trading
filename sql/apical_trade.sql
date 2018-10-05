8780b67442b26dfb4bd0399a0e35cff8a7281b8b
dmitry-meleshko <miscalculation@gmail.com>
2018-10-04 23:02:08 -0400

    Removing legacy

--- sql/apical_trade.sql
+++
@@ -1,3 +0,0 @@
-  create database apical_trade;
-
-  create table apical_trade.Quotes (symbol varchar(16), date date, open real, high real, low real, close real, volume int, constraint symbol_date primary key (symbol, date));
